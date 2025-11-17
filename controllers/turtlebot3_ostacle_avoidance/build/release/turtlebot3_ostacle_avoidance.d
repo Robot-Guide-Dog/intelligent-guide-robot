0ca8fb2d31766cfc2e509e2c7702f198b8140312
bek2-ful <sh.nea.yazid@gmail.com>
2025-11-17 13:56:10 +0000

    I have added camera device on the robot but in a different world

---
+++ controllers/turtlebot3_ostacle_avoidance/build/release/turtlebot3_ostacle_avoidance.d
@@ -0,0 +1,10 @@
+build/release/turtlebot3_ostacle_avoidance.o: \
+ turtlebot3_ostacle_avoidance.c \
+ C:\Robotics\Webots/include/controller/c/webots/lidar.h \
+ C:\Robotics\Webots/include/controller/c/webots/lidar_point.h \
+ C:\Robotics\Webots/include/controller/c/webots/types.h \
+ C:\Robotics\Webots/include/controller/c/webots/motor.h \
+ C:\Robotics\Webots/include/controller/c/webots/robot.h \
+ C:\Robotics\Webots/include/controller/c/webots/nodes.h \
+ C:\Robotics\Webots/include/controller/c/webots/camera.h \
+ C:\Robotics\Webots/include/controller/c/webots/camera_recognition_object.h
